df09044e9c4975ad26a1cf8c4dbb23623c351b2c
akrez <akrez.like@gmail.com>
2020-01-17 21:37:41 +0330

    sx

--- web/index.php
+++ web/index.php
@@ -99,6 +99,11 @@ $config = [
         ],
         'mailer' => $params['mailer'],
     ],
+    'modules' => [
+        'api' => [
+            'class' => 'app\modules\api\Module',
+        ],
+    ],
     'params' => $params['params'],
 ];
 
