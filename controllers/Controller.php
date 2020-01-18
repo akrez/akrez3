bc92748b051d93260845175a0c1557e0951ddf8c
akrez <akrez.like@gmail.com>
2020-01-18 21:40:59 +0330

    sdcsdc

--- controllers/Controller.php
+++ controllers/Controller.php
@@ -7,6 +7,7 @@ use Yii;
 use yii\helpers\Url;
 use yii\web\Controller as BaseController;
 use yii\web\ForbiddenHttpException;
+use yii\web\NotFoundHttpException;
 
 class Controller extends BaseController
 {
@@ -43,7 +44,7 @@ class Controller extends BaseController
             $userId = Yii::$app->user->getId();
             $blog = Blog::findBlogForAdmin($blogName, $userId);
             if ($blog == null) {
-                throw new ForbiddenHttpException(Yii::t('yii', 'You are not allowed to perform this action.'));
+                throw new NotFoundHttpException(Yii::t('yii', 'Page not found.'));
             }
             Yii::$app->blog->setIdentity($blog);
         }
--- modules/api/Module.php
+++ modules/api/Module.php
@@ -17,6 +17,15 @@ class Module extends \yii\base\Module
         Yii::$app->user->enableSession = false;
         Yii::$app->request->enableCsrfValidation = false;
         Yii::$app->response->format = Response::FORMAT_JSON;
+        Yii::$app->response->charset = 'UTF-8';
+        Yii::$app->response->on(Response::EVENT_BEFORE_SEND, function ($event) {
+            $response = $event->sender;
+            if ($response->data !== null && $response->statusCode !== 200) {
+                $code = $event->sender->statusCode;
+                $event->sender->data = ($code == 200 || YII_ENV_DEV ? (array) $event->sender->data : []);
+                $event->sender->data['code'] = $code;
+            }
+        });
     }
 
 }
